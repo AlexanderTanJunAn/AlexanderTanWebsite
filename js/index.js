e53db69bd93cc46f0426e25d292fee2bdf40fcac
AlexanderTanCS <alex1998tan@hotmail.com>
2021-06-25 10:25:06 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
-import smoothscroll from 'smoothscroll-polyfill';
-
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
-
-smoothscroll.polyfill();
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
